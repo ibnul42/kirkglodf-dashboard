99cde76e470d1e78b3ee92497c0460ae63073985
Ibnul Ashir <ibnulashir42@gmail.com>
2025-02-20 17:23:51 +0600

    fix

--- app/(authenticated)/dashboard/OverallInfo.jsx
+++ app/(authenticated)/dashboard/OverallInfo.jsx
@@ -14,42 +14,42 @@ export default function OverallInfo() {
                 <p className='text-sm text-[#9B9B9B]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro deserunt dolorum quasi dignissimos ipsum. Minus, distinctio. Esse perferendis autem architecto.</p>
             </article>
             <div className="">
-                <SingleInfo />
+                {/* <SingleInfo /> */}
             </div>
         </div>
     )
 }
 
-const SingleInfo = () => {
-    const icons = {
-        Entertainment: dynamic(() => import('@assets/entertainment.svg'), { ssr: false }),
-        Connectivity: dynamic(() => import('@assets/connectivity.svg'), { ssr: false }),
-        Health: dynamic(() => import('@assets/health.svg'), { ssr: false }),
-        Internet: dynamic(() => import('@assets/internet.svg'), { ssr: false }),
-    };
+// const SingleInfo = () => {
+//     const icons = {
+//         Entertainment: dynamic(() => import('@assets/entertainment.svg'), { ssr: false }),
+//         Connectivity: dynamic(() => import('@assets/connectivity.svg'), { ssr: false }),
+//         Health: dynamic(() => import('@assets/health.svg'), { ssr: false }),
+//         Internet: dynamic(() => import('@assets/internet.svg'), { ssr: false }),
+//     };
 
-    const allConnect = [
-        { name: 'Entertainment', value: '95%' },
-        { name: 'Connectivity', value: '80%' },
-        { name: 'Health', value: '90%' },
-        { name: 'Internet', value: '154', text: true },
-    ];
+//     const allConnect = [
+//         { name: 'Entertainment', value: '95%' },
+//         { name: 'Connectivity', value: '80%' },
+//         { name: 'Health', value: '90%' },
+//         { name: 'Internet', value: '154', text: true },
+//     ];
 
-    return (
-        <div className="flex gap-5">
-            {allConnect.map(({ name, value, text }) => {
-                const Icon = icons[name];
-                return (
-                    <div key={name} className="flex gap-2 lg:gap-4 items-center">
-                        <Icon className="w-6 h-auto" />
-                        <div>
-                            <p className="text-lg lg:text-2xl font-semibold">{value} {text && <span className='text-sm lg:text-lg font-medium'>MB</span>}</p>
-                            <p className="text-sm text-[#9B9B9B]">{name}</p>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
-};
+//     return (
+//         <div className="flex gap-5">
+//             {allConnect.map(({ name, value, text }) => {
+//                 const Icon = icons[name];
+//                 return (
+//                     <div key={name} className="flex gap-2 lg:gap-4 items-center">
+//                         <Icon className="w-6 h-auto" />
+//                         <div>
+//                             <p className="text-lg lg:text-2xl font-semibold">{value} {text && <span className='text-sm lg:text-lg font-medium'>MB</span>}</p>
+//                             <p className="text-sm text-[#9B9B9B]">{name}</p>
+//                         </div>
+//                     </div>
+//                 );
+//             })}
+//         </div>
+//     );
+// };
 
